e8d796b041a5aa45927ee7cfe1387c40579f0ad4
laurenmitchell <lauren.a.mitchell@colorado.edu>
2018-11-07 14:43:51 -0500

    updated html templates

--- mycode.js
+++ mycode.js
@@ -1,3 +1,9 @@
 //this is a javascript file
 
 // document.write("hello world");
+var adjective = prompt('Please type an adjective');
+var verb = prompt('Please type a verb that does not end in -ing ');
+var noun = prompt('Please type a noun');
+alert('All done. Ready for the message?');
+var sentence = "There once was a " + adjective +  " "+ verb + " " + noun
+console.log(sentence);
